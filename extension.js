6ee957634245f9ac70edab1d89d1a4d3a8f1510d
Grigory Petrov <grigory.v.p@gmail.com>
2018-08-12 14:09:52 +0300

    extension.xi.goBack command for going back to the previous Xi file

--- extension.js
+++ extension.js
@@ -2,8 +2,36 @@ const vscode = require('vscode');
 const geLinkProvider = require('./get_link_provider.js');
 
 
+const HISTORY_KEY = 'file-history';
+
+
 module.exports.activate = function(ctx) {
-  const reg = vscode.languages.registerDocumentLinkProvider;
+  const regProvider = vscode.languages.registerDocumentLinkProvider;
   const LinkProvider = geLinkProvider(vscode);
-  ctx.subscriptions.push(reg('xi', new LinkProvider()));
+  ctx.subscriptions.push(regProvider('xi', new LinkProvider()));
+  vscode.workspace.onDidOpenTextDocument(e => {
+    const filePath = e.fileName;
+    if (filePath.endsWith('.xi')) {
+      let history = ctx.globalState.get(HISTORY_KEY);
+      if (!Array.isArray(history)) history = [];
+      if (history.length > 100) history.pop();
+      history.push(filePath);
+      ctx.globalState.update(HISTORY_KEY, history);
+    }
+  });
+  const regCmd = vscode.commands.registerCommand;
+  ctx.subscriptions.push(regCmd('extension.xi.goBack', () => {
+    const history = ctx.globalState.get(HISTORY_KEY);
+    if (Array.isArray(history) && history.length > 1) {
+      //  Current file.
+      history.pop();
+      //  Previous file
+      const filePath = history.pop();
+      ctx.globalState.update(HISTORY_KEY, history);
+      const uri = vscode.Uri.file(filePath);
+      vscode.workspace.openTextDocument(uri).then(doc => {
+        vscode.window.showTextDocument(doc);
+      });
+    }
+  }));
 }
