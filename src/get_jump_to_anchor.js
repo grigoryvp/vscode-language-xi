b051df86cab90cd2c76222787c5b7d43a7cb06a2
Girogry Petrov <grigory.v.p@gmail.com>
2018-10-12 11:35:53 +0300

    Refactoring, wrong line jump fix

--- src/get_jump_to_anchor.js
+++ src/get_jump_to_anchor.js
@@ -1,15 +1,35 @@
 module.exports = function(vscode) {
 
+  // From MDN
+  function escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  }
 
-  return function(query) {
+
+  return function(anchor) {
     const editor = vscode.window.activeTextEditor;
     if (!editor) return;
     const doc = editor.document;
     const text = doc.getText();
-    const idx = (() => {
-      if (typeof(query) === 'string') return text.indexOf(query);
-      return text.search(query);
+
+    const query = (() => {
+      const needle = escapeRegExp(anchor);
+      if (anchor.endsWith('#')) {
+        const query = `\[${needle}\]`;
+        return new RegExp(query, 'im');
+      }
+      //! Can't use '\s' since it matches '\n' in multiline mode.
+      const link = `(\\[[^\\]]*\\])?`;
+      const selflink = `(\\[\\])?`;
+      const type = `([^ \\t][ \\t])?`;
+      const begin = `[ \\t]*${type}${link}[ \\t]*`;
+      const end = `${selflink}[ \\t]*${link}[ \\t]\\.`;
+      const query = `^${begin}${needle}${end}$`;
+      //  No 'g', first match.
+      return new RegExp(query, 'im');
     })();
+
+    const idx = text.search(query);
     if (idx === -1) return;
     const pos = doc.positionAt(idx);
     const range = new vscode.Range(pos, pos);
--- src/get_open_cmd.js
+++ src/get_open_cmd.js
@@ -5,12 +5,6 @@ module.exports = function(vscode) {
   const jumpToAnchor = getJumpToAnchor(vscode);
 
 
-  // From MDN
-  function escapeRegExp(string) {
-    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-  }
-
-
   return function(argmap) {
     if (!argmap) return;
 
@@ -18,7 +12,7 @@ module.exports = function(vscode) {
     //  Link like [#foo] to [#foo] anchor in same file.
     if (!file && anchor) {
       // jump to anchor like [foo#]
-      jumpToAnchor(`[${anchor}#]`);
+      jumpToAnchor(`${anchor}#`);
     }
     //  Link like [foo] or [foo#bar].
     else {
@@ -26,19 +20,8 @@ module.exports = function(vscode) {
       vscode.workspace.openTextDocument(uri).then(doc => {
         vscode.window.showTextDocument(doc).then(() => {
           if (!anchor) return;
-          const query = (() => {
-            const link = `(\\[[^\\]]*\\])?`;
-            const selflink = `(\\[\\])?`;
-            const type = `([^\\s]\\s)?`;
-            const needle = escapeRegExp(anchor);
-            const begin = `\\s*${type}${link}\\s*`;
-            const end = `${selflink}\\s*${link}\\s\\.`;
-            const query = `^${begin}${needle}${end}$`;
-            //  No 'g', first match.
-            return new RegExp(query, 'im');
-          })();
           // Search headings like `  * [link] some header[] [another link] .`
-          jumpToAnchor(query);
+          jumpToAnchor(anchor);
         });
       });
     }
