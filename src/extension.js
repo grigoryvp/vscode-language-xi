8c1fe55e17cef532708a7a756cc1f3fd00b99630
Girogry Petrov <grigory.v.p@gmail.com>
2018-10-12 09:43:46 +0300

    Refactoring

--- src/extension.js
+++ src/extension.js
@@ -7,13 +7,14 @@ const getOpenCmd = require('./get_open_cmd.js');
 
 module.exports.activate = function(ctx) {
   const HISTORY_KEY = 'file-history';
-  const regProvider = vscode.languages.registerDocumentLinkProvider;
   const LinkProvider = geLinkProvider(vscode);
   const onEditorChange = getEditorChangeHandler(ctx, HISTORY_KEY);
   const goBackCmd = getGoBackCmd(vscode, ctx, HISTORY_KEY);
   const openCmd = getOpenCmd(vscode);
-  ctx.subscriptions.push(regProvider('xi', new LinkProvider()));
+
   vscode.window.onDidChangeActiveTextEditor(onEditorChange);
+  const regProvider = vscode.languages.registerDocumentLinkProvider;
+  ctx.subscriptions.push(regProvider('xi', new LinkProvider()));
   const regCmd = vscode.commands.registerCommand;
   ctx.subscriptions.push(regCmd('extension.xi.goBack', goBackCmd));
   ctx.subscriptions.push(regCmd('extension.xi.open', openCmd));
