72a4be1f00609d16185f2f9e5453cb79a8f1c945
Girogry Petrov <grigory.v.p@gmail.com>
2018-11-16 20:47:32 +0300

    Configured xi path retrieval

--- src/get_lookup_cmd.js
+++ src/get_lookup_cmd.js
@@ -1,9 +1,31 @@
 const tools = require('./tools.js');
+const util = require('util');
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
+const p = util.promisify;
 
 
 module.exports = function(vscode) {
   return async () => {
+    const cfg = vscode.workspace.getConfiguration('xi');
+    let xiDir = cfg.lookupPath;
+    //  Something like "~/Documents/PowerShell/xi" on any OS.
+    if (!xiDir) {
+      return tools.error(tools.TXT_NO_LOOKUP_PATH);
+    }
+    xiDir = xiDir.replace(/^~/, `${os.homedir()}/`);
+    xiDir = xiDir.replace(/\//g, path.sep);
+    try {
+      const stat = await p(fs.stat)(xiDir);
+      if (!stat.isDirectory()) {
+        return tools.error(tools.TXT_NOT_DIR, xiDir);
+      }
+    }
+    catch (e) {
+      return tools.error(tools.TXT_NOT_DIR, xiDir);
+    }
+
     const res = await vscode.window.showQuickPick(["not implemented"]);
-    tools.debug(res);
   }
 }
--- src/tools.js
+++ src/tools.js
@@ -2,9 +2,22 @@ module.exports = {
   //  Unit test compatibility.
   _vscode: null,
 
+  TXT_NO_LOOKUP_PATH: `'xi.lookupPath' configuration not specified`,
+  TXT_NOT_DIR: `'{0}' is not a directory`,
+
   //  Wait some time for VSCode to fully load before using this.
   debug: (msg) => {
     if (!this._vscode) this._vscode = require('vscode');
     this._vscode.debug.activeDebugConsole.appendLine(msg);
+  },
+
+  //  Display error message with interpolation.
+  error: (msg, ...valSeq) => {
+    if (!this._vscode) this._vscode = require('vscode');
+    //  error("{0} not found", "foo") => "foo not found"
+    for (const [i, val] of Object.entries(valSeq)) {
+      msg = msg.replace(`{${i}}`, val);
+    }
+    this._vscode.window.showErrorMessage(msg);
   }
 };
