6e3b207eb12eb25c6e208db7b32758818f991251
Grigory Petrov <grigoryvp@gmail.com>
2024-01-02 19:19:53 +0100

    try to use the same code for both desktop and web versions

--- esbuild.mjs
+++ esbuild.mjs
@@ -2,18 +2,9 @@ import * as esbuild from 'esbuild';
 
 esbuild.build({
   bundle: true,
-  outfile: './out/extension-node.js',
-  entryPoints: ['./src/extension.mjs'],
-  external: ['vscode'],
-  format: 'cjs',
-  platform: 'node',
-});
-
-esbuild.build({
-  bundle: true,
-  outfile: './out/extension-browser.js',
+  outfile: './out/extension.js',
   entryPoints: ['./src/extension.mjs'],
   external: ['vscode', 'process', 'util', 'path', 'fs', 'os'],
   format: 'cjs',
-  platform: 'browser',
+  platform: 'neutral',
 });
